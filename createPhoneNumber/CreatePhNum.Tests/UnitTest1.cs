ae5b8cf5f92e79f0925b4aaf59343d0e482b9685
alstof <alstof@gmail.com>
2018-09-19 06:32:57 -0700

    Add project scaffolding

---
+++ createPhoneNumber/CreatePhNum.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using CreatePhNum;
+
+namespace Tests
+{
+  [TestFixture]
+  public class Tests
+  {
+    [Test]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, ExpectedResult = "(123) 456-7890")]
+    [TestCase(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, ExpectedResult = "(111) 111-1111")]
+    public static string FixedTest(int[] numbers)
+    {
+      return Class1.CreatePhoneNumber(numbers);
+    }
+  }
+}
---
+++ createPhoneNumber/CreatePhNum/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CreatePhNum
+{
+  public class Class1
+  {
+    public static string CreatePhoneNumber(int[] numbers)
+    {
+      return default(string);
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
