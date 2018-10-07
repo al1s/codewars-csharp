cce4ae8e44d675e6573428a2ee557d6c7f987b35
alstof <alstof@gmail.com>
2018-10-07 11:41:17 -0700

    Initial commit

---
+++ firstNonRepeatingLetter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace firstNonRepeatingLetter
+{
+  class Program
+  {
+    // https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/csharp
+    public static string FirstNonRepeatingLetter(string s)
+    {
+      string result = String.Empty;
+      return result;
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
