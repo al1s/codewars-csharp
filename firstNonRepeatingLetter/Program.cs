ea99b3cfac462db0e715c6332747590b92e17268
alstof <alstof@gmail.com>
2018-10-07 12:49:33 -0700

    Add solution

--- firstNonRepeatingLetter/Program.cs
+++ firstNonRepeatingLetter/Program.cs
@@ -1,18 +1,46 @@
-﻿using System;
-
-namespace firstNonRepeatingLetter
+﻿
+namespace Kata
 {
+  using System.Collections.Generic;
+  using System.Linq;
+  using Solution;
   class Program
   {
     // https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/csharp
     public static string FirstNonRepeatingLetter(string s)
     {
-      string result = String.Empty;
-      return result;
+      Dictionary<char, int> charFreq = s
+        .GroupBy(ch => char.ToUpperInvariant(ch))
+        .ToDictionary(grp => grp.Key, grp => grp.Count());
+      foreach (char ch in s)
+      {
+        if (charFreq[char.ToUpperInvariant(ch)] == 1) return ch.ToString();
+      }
+      return "";
     }
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      KataTest tests = new KataTest();
+      tests.BasicTests();
+    }
+  }
+}
+
+namespace Solution
+{
+  using NUnit.Framework;
+  using System;
+  using Kata;
+
+  [TestFixture]
+  public class KataTest
+  {
+    [Test]
+    public void BasicTests()
+    {
+      Assert.AreEqual("a", Program.FirstNonRepeatingLetter("a"));
+      Assert.AreEqual("t", Program.FirstNonRepeatingLetter("stress"));
+      Assert.AreEqual("e", Program.FirstNonRepeatingLetter("moonmen"));
     }
   }
 }
